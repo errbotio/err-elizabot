f85208d83cf18c15dab4c8e900ec974b8675a793
Guillaume BINET <gbin@gootz.net>
2012-05-21 15:05:07 +0200

    init commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+elizabot - a classic electronic shrimp for err
+==============================================
+
+For more information about err you can find it here: https://github.com/gbin/err
+
+**Installation**
+
+If you have the admin rights on an err chatbot simply use::
+    !install git@github.com:gbin/err-elizabot.git
+
+in order to install the plugin.
+Then !help to see the available commands and their explanation.
+
+
---
+++ elizaBot.py
@@ -0,0 +1,17 @@
+from botplugin import BotPlugin
+from less_serious_plugins.eliza import eliza
+from jabberbot import botcmd
+from utils import get_jid_from_message
+
+__author__ = 'gbin'
+
+class ElizaBot(BotPlugin):
+    eliza_daemon = eliza()
+    @botcmd
+    def eliza(self, mess, args):
+        """ El'cheapo shrink for you """
+        args = args.strip()
+        return self.eliza_daemon.respond(args)
+
+    def callback_message(self, conn, mess):
+        pass
