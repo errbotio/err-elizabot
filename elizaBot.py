dbb4f25ed75c7430a6d8ab13c0b326514f28e0eb
Guillaume BINET <gbin@gootz.net>
2012-06-09 14:05:42 +0200

    added the command !complete

--- elizaBot.py
+++ elizaBot.py
@@ -1,8 +1,10 @@
 import json
+from random import choice
+from lxml import objectify
 from errbot.botplugin import BotPlugin
 from eliza import Eliza
 from errbot.jabberbot import botcmd
-from urllib2 import urlopen
+from urllib2 import urlopen,quote
 
 __author__ = 'gbin'
 
@@ -17,8 +19,24 @@ class ElizaBot(BotPlugin):
     @botcmd
     def askus(self, mess, args):
         """ Give us a fun topic to talk about
+            thx to http://chatoms.com/
         """
         content = urlopen('http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7').read()
         return json.loads(content)['text']
 
+    @botcmd
+    def complete(self, mess, args):
+        """ Complete the given sentence
+            thx to the awesome google completion
+        """
+        args = args.strip()
+        if not args:
+            return 'Complete what ?'
+
+        content = urlopen('http://google.com/complete/search?q=%s&output=toolbar'%quote(args)).read()
+        xml = objectify.fromstring(content)
+        possibilities = xml.xpath("//toplevel/CompleteSuggestion/suggestion/@data")
+        if possibilities:
+            return choice(possibilities)
+        return 'Hmmm... no answer for that'
 
