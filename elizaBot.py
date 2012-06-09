e9905ac1e5ffae38c2bdb3acb098d6434c43c948
Guillaume BINET <gbin@gootz.net>
2012-06-09 13:33:21 +0200

    added the !askus command

--- elizaBot.py
+++ elizaBot.py
@@ -1,6 +1,8 @@
+import json
 from errbot.botplugin import BotPlugin
 from eliza import Eliza
 from errbot.jabberbot import botcmd
+from urllib2 import urlopen
 
 __author__ = 'gbin'
 
@@ -11,4 +13,12 @@ class ElizaBot(BotPlugin):
         """ El'cheapo shrink for you """
         args = args.strip()
         return self.eliza_daemon.respond(args)
+
+    @botcmd
+    def askus(self, mess, args):
+        """ Give us a fun topic to talk about
+        """
+        content = urlopen('http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7').read()
+        return json.loads(content)['text']
+
 
